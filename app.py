972235dd48c4eb7857178ae2000192f68a37fc25
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-06 23:49:43 +0530

    modified

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_geek():
-    return 'ok done!!!!!!!!!, added webhook'
+    return 'ok python app is done p!!!!!!!!!, added webhook'
 @app.route('/hi')
 def hell():
     return '<h1>Hiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiii from Flask & Docker</h1>'
